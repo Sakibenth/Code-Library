a9e961898506cab5fa70ce9eebb6f59387e2e5d0
Sadman Sakib <shadmanshakib982@gmail.com>
2026-03-29 08:19:57 +0600

    Sollution of power of two is added

---
+++ Power_of_two.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool power_of_two(int n){
+    if(n==0){
+        return 1;
+    }
+    if((n & n-1)==0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << power_of_two(n) << '\n';
+    return 0;
+}
