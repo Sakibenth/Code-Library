1aeef97b96817933d08046832e3796a0b2557ca0
Sadman Sakib <shadmanshakib982@gmail.com>
2026-04-23 10:33:53 +0600

    explanation of max_sum_subarray added

--- maximum_subarray_sum.cpp
+++ maximum_subarray_sum.cpp
@@ -9,6 +9,17 @@ int main(){
     for(int i=0;i<n;i++){
         cin >> a[i];
     }
+    /*This approach involves prefix sum array.
+    Let an array be of size 2---->[-1,2]
+    one array ending at index 1([1]). Two array ending at index 2([-1],[-1,2])
+    max sum of the array ending at 1 is -1
+    max sum of the array ending at 2 is 1
+    prefix_sum[1]=p[1]
+    prefix_sum[2]=p[2]
+    max_sum[2]=prefix_sum[2]-(min(p[1],p[0]))
+    max_sum[i]=prefix_sum[i]-(min(p[0],p[1],p[2],....p[i-1]))
+    maximum of all max_sum at i index will be the maximum sum of the sub_array
+    */
     long long max_sum=-10e9;
     long long min_p_sum=0;
     long long prefix_sum=0;
